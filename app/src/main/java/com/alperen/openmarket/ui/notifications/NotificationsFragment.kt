c378045b8cd1bd4977666d7df888825a98f2207e
Alperen Erdoğan <alperen.erdogan.neu@gmail.com>
2021-10-28 22:26:53 +0300

    notifications page

--- app/src/main/java/com/alperen/openmarket/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/alperen/openmarket/ui/notifications/NotificationsFragment.kt
@@ -5,15 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.alperen.openmarket.R
+import com.alperen.openmarket.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notifications, container, false)
+    ): View {
+        val binding = FragmentNotificationsBinding.inflate(inflater)
+
+        with(binding) {
+            btnBack.setOnClickListener {
+                root.findNavController().popBackStack()
+            }
+
+            val list = arrayListOf("a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a",)
+            recyclerNotifications.adapter = NotificationsRecyclerViewAdapter(list)
+            recyclerNotifications.layoutManager = LinearLayoutManager(root.context)
+
+            return root
+        }
     }
 }
